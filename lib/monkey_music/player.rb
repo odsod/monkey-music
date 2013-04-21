5b3aee9ad81798562f029a9aa447afac00fd845f
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2013-04-21 23:09:38 +0200

    implement test program, which always moves monkey toward closest thing

--- lib/monkey_music/player.rb
+++ lib/monkey_music/player.rb
@@ -10,6 +10,8 @@ module MonkeyMusic
     def query_move!
       IO.popen(@file, "r+") do |io|
         io.puts @monkey.serialize
+        io.puts @monkey.level.width
+        io.puts @monkey.level.height
         io.puts @monkey.level.serialize
         @next_move = parse_move(io.gets.chomp)
       end
