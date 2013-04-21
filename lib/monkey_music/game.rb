98e59dee17f21f3db1eb8c738cdaf2b93f04a2d4
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2013-04-21 15:10:31 +0200

    add option parsing for user generation

--- lib/monkey_music/game.rb
+++ lib/monkey_music/game.rb
@@ -30,18 +30,30 @@ module MonkeyMusic
 
     def parse_options
       opt_parser = OptionParser.new do |opts|
-        opts.banner = "Usage: monkeymusic level [options]"
+        opts.banner = "Usage: monkeymusic [options]"
         opts.on('-l', '--level LEVEL', "The level to play.") do |level_name|
           @level_name = level_name;
         end
-        opts.on('--player FILE', "The path to a player program.") do |file|
+        opts.on('-g', '--generate-user USER', "Generate music recommendations for a Spotify user.") do |user|
+          @generate_user = user
+        end
+        opts.on('-f', '--player-file FILE', "The path to a player program.") do |file|
           @players << Player.new(file)
         end
-        opts.on('--name NAME', "Set the name of the last entered player.") do |name|
+        opts.on('-n', '--player-name NAME', "Set the name of the last entered player.") do |name|
           @players[-1].monkey.name = name
         end
-        opts.on('--user USER', "The user to get recommendations from.") do |user|
+        opts.on('-u', '--user USER', "The user to get recommendations from.") do |user|
           @user_name = user
+        end
+        opts.on('-k', '--app-key KEY', "Path to libspotify application key.") do |key|
+          @app_key = File.join(Dir.getwd, key)
+        end
+        opts.on('-a', '--account ACCOUNT', "Username for a Spotify premium account.") do |account|
+          @account = account
+        end
+        opts.on('-p', '--password PASSWORD', "Password for a Spotify premium account.") do |password|
+          @password = password
         end
         opts.on_tail('-h', '--help', "Show this message.") do 
           puts opts
@@ -50,7 +62,7 @@ module MonkeyMusic
       end
       opt_parser.parse!(@arguments)
       if !@user_name || !@level_name
-        puts opts
+        puts opt_parser
         exit
       end
     end
