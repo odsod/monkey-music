2233b8c9f41987b838b5eefb24f30205ae59a210
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2013-04-27 22:33:00 +0200

    move unit and level classes into subfolders

--- lib/monkey_music.rb
+++ lib/monkey_music.rb
@@ -2,16 +2,20 @@ $: << File.dirname(__FILE__)
 
 require 'set'
 
+require 'monkey_music/runner'
+require 'monkey_music/config'
 require 'monkey_music/game'
 require 'monkey_music/player'
-require 'monkey_music/level'
-require 'monkey_music/level_loader'
-require 'monkey_music/user.rb'
-require 'monkey_music/user_generator.rb'
-require 'monkey_music/user_hallon_loader.rb'
 
-require 'monkey_music/ui/console_ui.rb'
-require 'monkey_music/ui/browser_ui.rb'
+require 'monkey_music/levels/level'
+require 'monkey_music/levels/level_loader'
+
+require 'monkey_music/users/user'
+require 'monkey_music/users/user_generator'
+require 'monkey_music/users/user_hallon_loader'
+
+require 'monkey_music/ui/console_ui'
+require 'monkey_music/ui/browser_ui'
 
 require 'monkey_music/units/base'
 require 'monkey_music/units/monkey'
