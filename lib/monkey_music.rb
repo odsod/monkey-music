4ae07bbcfcc0b65e5aff5320d857334b6ee10ab7
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2013-04-28 16:24:49 +0200

    remove obsolete include from main file

--- lib/monkey_music.rb
+++ lib/monkey_music.rb
@@ -12,7 +12,6 @@ require 'monkey_music/levels/level_loader'
 
 require 'monkey_music/users/user'
 require 'monkey_music/users/user_generator'
-require 'monkey_music/users/user_hallon_loader'
 
 require 'monkey_music/ui/console_ui'
 require 'monkey_music/ui/browser_ui'
