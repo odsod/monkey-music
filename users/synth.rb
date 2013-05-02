9ed4db3caea7dd9d7a9982106b7d6ef3aca481ba
Oscar Söderlund <poscar@spotify.com>
2013-05-02 21:51:27 +0200

    add uri for disliked list on test user

--- users/synth.rb
+++ users/synth.rb
@@ -4,4 +4,4 @@ toplist :artists, "spotify:user:poscar%21:playlist:6MjfrIbeCKQMVCU9JQNf1u"
 
 toplist :albums, "spotify:user:poscar%21:playlist:1ZXPeNHa8YYvY0VzxRdv3q"
 
-toplist :disliked, ""
+toplist :disliked, "spotify:user:poscar%21:playlist:5IEx3cooSMgrtftvw5B9Ug"
