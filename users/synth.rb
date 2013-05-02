93980b24fdb00960facf38098c5af934f61bd087
Oscar Söderlund <poscar@spotify.com>
2013-05-02 20:15:36 +0200

    fill in test user playlist uris

--- users/synth.rb
+++ users/synth.rb
@@ -1,5 +1,5 @@
-list :top_tracks, "spotify:"
+list :top_tracks, "spotify:user:poscar%21:playlist:4yIhuHviCmTYjcFqunPxsa"
 
-list :top_artists, "spotify:"
+list :top_artists, "spotify:user:poscar%21:playlist:6MjfrIbeCKQMVCU9JQNf1u"
 
-list :top_albums, "spotify:"
+list :top_albums, "spotify:user:poscar%21:playlist:1ZXPeNHa8YYvY0VzxRdv3q"
