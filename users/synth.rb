3e4c017ba496e22c3066784b3792db6331a53b28
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2013-05-02 20:10:09 +0200

    move toplists to actual spotify playlists

--- users/synth.rb
+++ users/synth.rb
@@ -1,37 +1,5 @@
-toplist(:tracks, {
-  "Condemnation" => "spotify:track:5H85hOp2oMlhMh9JlkdJP2",
-  "The Fog Rose High" => "spotify:track:3NwaVr6RJg2LPBcJ1Ysl1O",
-  "Cars" => "spotify:track:3CKLPDN1AIOYa7QPpy6oDH",
-  "Tainted Love" => "spotify:track:1tVzVPIcCFz5QUMGLPHJEc",
-  "You Surround Me" => "spotify:track:3aTRuYGOehN1ux3HpKO9HD",
-  "Enola Gay" => "spotify:track:3WIg3fd3W8I5HU8g3Y6P0k",
-  "Metal" => "spotify:track:2gsY0pB4cJOVX32K3qOZgB",
-  "Something New" => "spotify:track:0GqRSiO3OF89bNwe9V8HYi",
-  "Guilty Partner" => "spotify:track:0gksEnBiPqADMHtLdwgvue",
-  "The New Stone Age" => "spotify:track:4P9BeQkwwcWIuioP8YzeQD",
-})
+list :top_tracks, "spotify:"
 
-toplist(:albums, {
-  "Exciter" => "spotify:track:16cLpj8F2zKUNdsLwuGYQs",
-  "The Pleasure Principle" => "spotify:track:2gsY0pB4cJOVX32K3qOZgB",
-  "Dare!" => "spotify:track:3L7RtEcu1Hw3OXrpnthngx",
-  #"Non-Stop Erotic Cabaret" => "",
-  #"Vienna" => "",
-  #"The Hurting" => "",
-  #"Rio" => "",
-  #"604" => "",
-  #"Maiden Voyage" => "",
-  #"Telekon" => "",
-})
+list :top_artists, "spotify:"
 
-toplist(:artists, {
-  #"Erasure" => "spotify:artist:0z5DFXmhT4ZNzWElsM7V89",
-  #"Gary Numan" => "spotify:artist:5KQMtyPE8DCQNUzoNqlEsE",
-  #"Depeche Mode" => "spotify:artist:762310PdDnwsDxAQxzQkfX",
-  #"Orchestral Manoeuvres In The Dark" => "spotify:artist:7wJ9NwdRWtN92NunmXuwBk",
-  #"Alice in Videoland" => "spotify:artist:6QNJlkUN2AijdGBQvBwFPj",
-  #"Soft Cell" => "spotify:artist:6aq8T2RcspxVOGgMrTzjWc",
-  #"New Order" => "spotify:artist:0yNLKJebCb8Aueb54LYya3",
-  #"Ladytron" => "spotify:artist:0ucLPotcQNI7AViFytdhBz",
-  #"Anoraak" => "spotify:artist:2DfP0hQCCFmZHjstSE8yXC",
-})
+list :top_albums, "spotify:"
