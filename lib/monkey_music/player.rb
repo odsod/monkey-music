1917cc643a192e54ec184630077b39765a988a24
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2013-05-09 03:27:45 +0200

    implement penalty when time limit exceeded

--- lib/monkey_music/player.rb
+++ lib/monkey_music/player.rb
@@ -18,10 +18,11 @@ module MonkeyMusic
     end
 
     def query!(turn)
-      if @penalty > 1
+      if @penalty > 0
         @penalty -= 1
-        @remaining_time = @monkey.level.time_limit if @penalty == 0
+        @remaining_time = @monkey.level.time_limit if @penalty < 1
       else
+        STDOUT.sync = true # Flush?
         IO.popen(@file, "r+") do |io|
           io.puts turn_output(turn)
           @remaining_time -= (Benchmark.realtime { @input = io.gets } * 1000).round
